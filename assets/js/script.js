9ad603e215bbe8b3aa96b948c2e0fd2758a868f0
Nikola Iliev <nikola.r.iliev.91@gmail.com>
2021-02-10 15:00:07 +0200

    fixing some CSS issues in header section, adding the rest content in options panel

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const searchBox = document.querySelector('.searchbox');
 const searchBar = document.getElementById('search');
 const searchForm = document.getElementById('searchForm');
 const submitBtn = document.getElementById('submitBtn');
+const profileNav = document.getElementsByClassName('profile-nav')[0];
+const profileIcon = document.getElementsByClassName('profile-icon')[0];
 searchBar.addEventListener('focus', () => {
     searchBox.style.display = 'block';
     searchForm.style.borderBottomLeftRadius = 0;
@@ -10,11 +12,15 @@ searchBar.addEventListener('focus', () => {
     searchForm.style.borderBottom = 'none';
     submitBtn.style.borderBottomRightRadius = 0;
     searchForm.style.backgroundColor = 'white';
+    document.body.style.pointerEvents = 'none';
+    searchForm.style.pointerEvents = 'all';
+    searchBox.style.pointerEvents = 'all';
     searchBar.addEventListener('focusout', () => {
         searchBox.style.display = 'none';
         searchForm.style.borderBottom = '1px solid #0082e6';
         searchForm.style.borderBottomLeftRadius = '25px';
         searchForm.style.borderBottomRightRadius = '25px';
         submitBtn.style.borderBottomRightRadius = '25px';
+        document.body.style.pointerEvents = 'all';
     });
 });
