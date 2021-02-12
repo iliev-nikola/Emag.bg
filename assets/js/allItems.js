46b31905f350e173d2e010854cb0aafc13433a3b
Your name here <mtdamyanova@gmail.com>
2021-02-13 00:49:09 +0200

    adding items of FOCUS-BAR SECTION

---
+++ assets/js/allItems.js
@@ -0,0 +1,147 @@
+//ITEMS IN FOCUS-BAR SECTION
+const allFocusItems = [
+    {
+        'id': 1,
+        'title': 'Смартфон Xiaomi Redmi 9C NFC, Dual SIM, 32GB, 4G, Midnight Gray',
+        'image': './assets/images/focus-items/1.jpg',
+        'currentPrice': 199,
+        'regularPrice': '-'
+
+    },
+    {
+        'id': 2,
+        'title': 'Телевизор Star-Light 32DM6700, 32" (81 см), Smart Android, HD, LED, Class A+',
+        'image': './assets/images/focus-items/2.jpg',
+        'currentPrice': 294,
+        'regularPrice': '-'
+
+    },
+    {
+        'id': 3,
+        'title': 'Пералня Indesit MTWE71252WEE, 7 кг, 1200 об/мин, Клас A+++, MyTime, Fast Cycles, Бял',
+        'image': './assets/images/focus-items/3.jpg',
+        'currentPrice': 539,
+        'regularPrice': '-'
+
+    },
+    {
+        'id': 4,
+        'title': 'Акумулаторна ударна бормашина 18V 2.0Ah Li-Ion Steinhaus',
+        'image': './assets/images/focus-items/4.jpg',
+        'currentPrice': 89,
+        'regularPrice': '-'
+
+    }
+    ,
+    {
+        'id': 5,
+        'title': 'Електрическа тротинетка KugooS1 Pro, Автономия 30 км',
+        'image': './assets/images/focus-items/5.png',
+        'regularPrice': 849,
+        'currentPrice': 589
+
+    },
+    {
+        'id': 6,
+        'title': 'Детска количка Kinderkraft 3 in 1 XMoov, Черна ',
+        'image': './assets/images/focus-items/6.jpg',
+        'currentPrice': 569,
+        'regularPrice': 799
+
+    },
+    {
+        'id': 7,
+        'title': 'Apple Watch 5, GPS, Cellular, 44mm Space Black Stainless Steel Case with Black Sport Band - S/M & M/L ',
+        'image': './assets/images/focus-items/7.png',
+        'currentPrice': 1199,
+        'regularPrice': '-'
+    },
+    {
+        'id': 8,
+        'title': 'Кабел A+ High-Speed HDMI 1.4V, plug-plug, Ethernet, gold-plated, 5 м',
+        'image': './assets/images/focus-items/8.png',
+        'currentPrice': 9,
+        'regularPrice': '-'
+
+    },
+    {
+        'id': 9,
+        'title': 'Робот прахосмукачка Xiaomi Mi Robot Vacuum Mop Cleaner, 0.6 л, WI-FI',
+        'image': './assets/images/focus-items/9.png',
+        'currentPrice': 439,
+        'regularPrice': 599
+
+    },
+    {
+        'id': 10,
+        'title': 'Комплект мивка algranit Alveus Cancan 30, 580x420 мм + Смесител Diana, Бежов',
+        'image': './assets/images/focus-items/10.png',
+        'currentPrice': 389,
+        'regularPrice': '-'
+    },
+    {
+        'id': 11,
+        'title': 'Автобокс Dacia, Твърд, 480 л, Двойно отваряне, Лъскаво черно, Лого Dacia',
+        'image': './assets/images/focus-items/11.png',
+        'currentPrice': 549,
+        'regularPrice': 679
+
+    },
+    {
+        'id': 12,
+        'title': 'Слушалки In-Ear Wraps CSUN-VI5M, Stereo Talk, Оранжев',
+        'image': './assets/images/focus-items/12.png',
+        'currentPrice': 13,
+        'regularPrice': '-'
+
+    },
+    {
+        'id': 13,
+        'title': 'Аудио слушалки A+ SBG5, Bluetooth, Черни',
+        'image': './assets/images/focus-items/13.png',
+        'currentPrice': 44,
+        'regularPrice': '-'
+
+    },
+    {
+        'id': 14,
+        'title': 'Ел. четка за зъби Oral B Vitality D100 Sensi Ultra Thin + Travel Case , 1 глава, Бял',
+        'image': './assets/images/focus-items/14.png',
+        'currentPrice': 39,
+        'regularPrice': '-'
+
+    },
+    {
+        'id': 15,
+        'title': 'Комплект LED лента RGB Phenom, С дистанционно управление, 22W, 1260 lm, 3 м',
+        'image': './assets/images/focus-items/15.png',
+        'currentPrice': 39,
+        'regularPrice': 54
+
+    },
+    {
+        'id': 16,
+        'title': 'Камера за автомобил Prestigio RoadRunner 155, 2.0" display, FHD 30fps, 140°-градусов ъгъл, OVP, NTC, Датчик за движение, G-sensor',
+        'image': './assets/images/focus-items/16.png',
+        'currentPrice': 72,
+        'regularPrice': 84
+
+    }
+    ,
+    {
+        'id': 17,
+        'title': 'Клавиатура Apple Magic за iPad Pro 11" (2020), Layout INT EN, Black',
+        'image': './assets/images/focus-items/17.png',
+        'currentPrice': 749,
+        'regularPrice': '-'
+
+    },
+    {
+        'id': 18,
+        'title': 'Транспортна количка Wolfcraft 5520000 TS 1000, Максимално натоварване 200 кг',
+        'image': './assets/images/focus-items/18.png',
+        'currentPrice': 444,
+        'regularPrice': 594
+
+    },
+]
