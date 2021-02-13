1ddeb34e58f1bd5dcbabe319dd7986fb30f9c763
Your name here <mtdamyanova@gmail.com>
2021-02-13 11:57:12 +0200

    adding functionality-ADDING/DELETING ITEMS- HISTORY SECTION+HTML&CSS

--- assets/js/createItem.js
+++ assets/js/createItem.js
@@ -10,20 +10,28 @@ function createNewElement(type, text) {
 class Items {
     constructor() {
         this.allItems = [];
+        this.watchedItems = [];
     }
     addProduct(item) {
         this.allItems.push(item);
+    }
+    addWatched(item) {
+        if (this.watchedItems.length === 3) {
+            this.watchedItems.shift();
+        }
+        this.watchedItems.push(item);
     }
 }
 let focusSectionItems = new Items();
 allFocusItems.forEach(function (item) {
     focusSectionItems.addProduct(item);
-})
+});
 //CREATING ITEMS
 let cardsContainer = document.getElementById('cards-container');
+let watchedItems = document.getElementById('watched-items');
 function createItemsCard(array, container) {
     container.innerHTML = '';
-    array.forEach(function (currenItem) {
+    array.forEach(function (currentItem) {
         let cardContainer = createNewElement('div');
         cardContainer.className = 'items-card';
         let addFavourite = createNewElement('i');
@@ -33,17 +41,18 @@ function createItemsCard(array, container) {
         let imageContainer = createNewElement('a');
         imageContainer.href = '#';
         let itemImage = createNewElement('img');
-        itemImage.src = currenItem.image;
+        itemImage.src = currentItem.image;
         let sup = createNewElement('sup', '99');
         let valute = createNewElement('small', 'лв');
         let titleContainer = createNewElement('a');
         titleContainer.href = '#';
-        let itemTitle = createNewElement('h5', currenItem.title);
+        let itemTitle = createNewElement('h5', currentItem.title);
         let regular = createNewElement('div');
         regular.className = 'regular-price';
-        let regPrice = createNewElement('strong', currenItem.regularPrice);
-        let itemPrice = createNewElement('span', currenItem.currentPrice);
-        let percentage = Math.floor(100 - 100 * (currenItem.currentPrice / currenItem.regularPrice));
+        let regPrice = createNewElement('strong', currentItem.regularPrice);
+        regPrice.className = 'line';
+        let itemPrice = createNewElement('span', currentItem.currentPrice);
+        let percentage = Math.floor(100 - 100 * (currentItem.currentPrice / currentItem.regularPrice));
         let percentageBar = createNewElement('div', `-${percentage}%`);
         let sale = createNewElement('b', `(-${percentage}%)`);
         percentageBar.className = 'percentage';
@@ -51,21 +60,58 @@ function createItemsCard(array, container) {
         addFavourite.addEventListener("click", function () {
             addFavourite.className = 'fas fa-heart fav';
             addFavourite.style.color = "red";
-        })
+        });
         itemPrice.append(sup, valute);
         container.append(cardContainer);
         imageContainer.append(itemImage);
         titleContainer.append(itemTitle);
         cardContainer.append(imageContainer, titleContainer, itemPrice, addFavourite, addShoppingCard, percentageBar, regular);
-        if (currenItem.regularPrice === '-') {
+        if (currentItem.regularPrice === '-') {
             regular.style.visibility = 'hidden';
             percentageBar.style.display = 'none';
         } else {
             let sup = createNewElement('sub', '99');
+            sup.style.textDecoration = 'line-through';
             let valute = createNewElement('small', `лв `);
+            valute.className = 'line';
             regular.append(sup, valute, sale);
         }
+        let watchedContainer = document.getElementById('history-section');
+        //ADDING ITEMS IN HISTORY SECTION
+        function watchedItem(array) {
+            watchedContainer.style.display = 'block';
+            watchedItems.innerHTML = '';
+            if (array.length === 12) {
+                array.pop();
+                array.unshift(currentItem.image);
+            }
+            else {
+                array.push(currentItem.image);
+            }
+            array = new Set(array);
+            array = Array.from(array);
+            array.forEach(function (item) {
+                let mainContainer = createNewElement('div');
+                let watchedImage = createNewElement('img');
+                watchedImage.className = 'watched-images';
+                watchedImage.src = item;
+                watchedItems.append(mainContainer);
+                mainContainer.append(watchedImage);
+            });
+        }
+        titleContainer.addEventListener('click', function () {
+            watchedItem(focusSectionItems.watchedItems);
+        })
+        imageContainer.addEventListener('click', function () {
+            watchedItem(focusSectionItems.watchedItems);
+        })
+        //DELETE ITEMS FROM HISTORY SECTION
+        let deleteWatched = document.getElementById('delete-watched');
+        deleteWatched.addEventListener('click', function () {
+            watchedContainer.style.display = 'none';
+            focusSectionItems.watchedItems = [];
+        })
     });
-}
+};
 //ITEMS IN FOCUS-BAR SECTION
 createItemsCard(focusSectionItems.allItems, cardsContainer);
