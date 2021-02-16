4e38e320f3275f6e22ed93a0dfcb0fdcbb7d768c
Nikola Iliev <nikola.r.iliev.91@gmail.com>
2021-02-16 16:44:34 +0200

    fixing some functionality

--- assets/js/radioButtons.js
+++ assets/js/radioButtons.js
@@ -2,14 +2,20 @@ function createDots(array, container) {
     for (let i = 0; i < array.length / 6; i++) {
         let dot = createNewElement('input');
         dot.type = 'radio';
-        dot.name = 'radio-button';
+        if (container.id === 'dots-container') {
+            dot.name = 'dots-container';
+        } else {
+            dot.name = 'other-dots-container';
+        }
+
         dot.className = 'dots';
         dot.value = i + 1;
         container.append(dot);
-        if (dot.value === i) {
+        if (i === 0) {
             dot.checked = true;
         }
     }
 }
+
 createDots(focusSectionItems.allItems, DOTS_CONTAINER);
 createDots(otherWatched.allItems, DOTS_CONTAINER_OTHER);
