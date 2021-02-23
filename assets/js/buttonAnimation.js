dff9487a6a35c48baa9770db360dcee42bd90d34
Your name here <mtdamyanova@gmail.com>
2021-02-24 00:05:26 +0200

    adding functionality for radio buttons

--- assets/js/buttonAnimation.js
+++ assets/js/buttonAnimation.js
@@ -1,5 +1,5 @@
 //BUTTONS ANIMATION IN FOCUS-BAR SECTION AND OTHER CLIENTS WATCHED SECTION
-function scrollItems(array, rightScroll, leftScroll, container) {
+function scrollItems(array, rightScroll, leftScroll, container, mainContainer) {
     let counterFocus = 0;
     let counterOther = 0;
     let lengthScroll = 100 * ((array.length) / 6 - 1);
@@ -15,20 +15,19 @@ function scrollItems(array, rightScroll, leftScroll, container) {
                 container.classList.remove('blur');
                 totalAmount += scrollAmount;
             }
-
             if (totalAmount >= lengthScroll) {
                 rightScroll.style.display = 'none';
             }
-
             if (container.scrollLeft > 0) {
                 leftScroll.style.display = 'block';
             }
         }, 30);
 
-        if (container === CARDS_CONTAINER) {
+        if (leftScroll && container === CARDS_CONTAINER) {
             counterFocus++;
             ALL_RADIO_BUTTONS[counterFocus].checked = true;
-        } else {
+        }
+        if (leftScroll && container === OTHER_WATCHED_CONTAINER) {
             counterOther++;
             ALL_OTHER_BUTTONS[counterOther].checked = true;
         }
@@ -55,27 +54,53 @@ function scrollItems(array, rightScroll, leftScroll, container) {
             }
         }, 30);
 
-        if (container === CARDS_CONTAINER) {
+        if (rightScroll && container === CARDS_CONTAINER) {
             counterFocus--;
             ALL_RADIO_BUTTONS[counterFocus].checked = true;
-        } else {
+        }
+        if (rightScroll && container === OTHER_WATCHED_CONTAINER) {
             counterOther--;
             ALL_OTHER_BUTTONS[counterOther].checked = true;
         }
     }
 
+    mainContainer.addEventListener('click', function (ev) {
+        let counter;
+        if (container === CARDS_CONTAINER) {
+            counter = counterFocus + 1;
+        }
+        if (container === OTHER_WATCHED_CONTAINER) {
+            counter = counterOther + 1;
+        }
+        if (ev.target.tagName === 'INPUT' && counter < parseInt(ev.target.value)) {
+            for (let i = 0; i < parseInt(ev.target.value) - counter; i++) {
+                scrollLeft();
+            }
+            ev.target.checked = true;
+            counter = ev.target.value;
+        }
+        if (ev.target.tagName === 'INPUT' && counter > parseInt(ev.target.value)) {
+            for (let i = 0; i < counter - parseInt(ev.target.value); i++) {
+                scrollRight();
+            }
+            ev.target.checked = true;
+            counter = ev.target.value;
+        }
+    });
+
     rightScroll.addEventListener('click', scrollLeft);
     leftScroll.addEventListener('click', scrollRight);
 }
 
-scrollItems(ALL_FOCUS_ITEMS, RIGHT_SCROLL, LEFT_SCROLL, CARDS_CONTAINER);
-scrollItems(OTHER_CLIENTS_WATCHED, RIGTH_OTHER_SCROLL, LEFT_OTHER_SCROLL, OTHER_WATCHED_CONTAINER);
+scrollItems(ALL_FOCUS_ITEMS, RIGHT_SCROLL, LEFT_SCROLL, CARDS_CONTAINER, FOCUS_BAR);
+scrollItems(OTHER_CLIENTS_WATCHED, RIGTH_OTHER_SCROLL, LEFT_OTHER_SCROLL, OTHER_WATCHED_CONTAINER, DOTS_CONTAINER_OTHER);
 
 // SLIDESHOW IMAGES IN MAIN
+let radioBtns = Array.from(RADIO_MAIN);
 let currentSlide = 0;
 function slideShow() {
     CURRENT_IMAGE.src = slideImages[currentSlide];
-    RADIO_MAIN[currentSlide].checked = true;
+    radioBtns[currentSlide].checked = true;
 }
 
 setInterval(function () {
@@ -101,3 +126,13 @@ CHEVRON_LEFT.addEventListener('click', () => {
     }
     slideShow();
 });
+
+radioBtns.forEach(radio => {
+    radio.addEventListener('click', () => {
+        currentSlide = radio.value;
+        slideShow();
+    });
+});
+
+
+
